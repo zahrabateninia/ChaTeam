cde665b0a5bf1683f8e00e760523b7ccf2fe03d4
zahrabateninia <zahrabatenin@gmail.com>
2025-12-23 22:09:11 +0330

    Generate token with JWS

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
+
+export const generateToken = (userId, res) => {
+  const { JWT_SECRET } = ENV;
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not configured");
+  }
+
+  const token = jwt.sign({ userId }, JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks: cross-site scripting
+    sameSite: "strict", // CSRF attacks
+    secure: ENV.NODE_ENV === "development" ? false : true,
+  });
+
+  return token;
+};
+
+// http://localhost
+// https://someDomain.com in production it is secured 
