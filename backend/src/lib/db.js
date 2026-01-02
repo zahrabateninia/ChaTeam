bc42292c84dd11a8eda9a30b8abb6f1b0b01fa3d
zahrabateninia <zahrabatenin@gmail.com>
2026-01-02 21:33:48 +0330

    Check if MONGO_URL is set, then connect to it.

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose"
 
 export const connectDB = async() => {
     try {
+        const { MONGO_URL} = process.env;
+        if (!MONGO_URL) throw new Error("MONGO_URL is not set")
         const conn = await mongoose.connect(process.env.MONGO_URL)
         console.log("MONGODB CONNECTED", conn.connection.host)
         
