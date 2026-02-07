a3e756059458138bbb2b19dede9d1f5bd05ac90a
zahrabateninia <zahrabatenin@gmail.com>
2026-02-07 23:05:28 +0330

    Implement login functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -62,3 +62,34 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.error("Error in login controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logout = async (req, res) => {};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,16 +1,12 @@
-import express from 'express'
-import { signup } from '../controllers/auth.controller.js'
+import express from "express";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/signup',signup)
+router.post("/signup", signup);
 
-router.get('/login', (req,res) => {
-    res.send("login endpoint")
-})
+router.post("/login", login);
 
-router.get(('/logout'), (req,res) => {
-    res.send("logout endpoint")
-})
+router.post(("/logout", logout));
 
 export default router;
