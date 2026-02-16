d6b8a62bbc624e6d7452e66099f3265cda028015
zahrabateninia <zahrabatenin@gmail.com>
2026-02-16 21:50:20 +0330

    Implement logout functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -91,5 +91,7 @@ export const login = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
-
-export const logout = async (req, res) => {};
+export const logout = (_, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "Logged out successfully" });
+};
