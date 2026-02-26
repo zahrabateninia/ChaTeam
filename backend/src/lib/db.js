eb22cc7aa68123cb6c22c632219b454424dcfb82
zahrabateninia <zahrabatenin@gmail.com>
2026-02-26 23:40:18 +0330

    fix: Correct file format and change URL to URI

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,16 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import { ENV } from "./env";
 
-export const connectDB = async() => {
-    try {
-        const { MONGO_URL} = ENV;
-        if (!MONGO_URL) throw new Error("MONGO_URL is not set")
-        const conn = await mongoose.connect(ENV.MONGO_URL)
-        console.log("MONGODB CONNECTED", conn.connection.host)
-        
-    } catch (error) {
-        console.error("ERROR CONNECTING TO MONGODB:", error)
-        process.exit(1)   
-    }
-}
-
+export const connectDB = async () => {
+  try {
+    const { MONGO_URI } = ENV;
+    if (!MONGO_URI) throw new Error("MONGO_URI is not set");
+    const conn = await mongoose.connect(ENV.MONGO_URI);
+    console.log("MONGODB CONNECTED", conn.connection.host);
+  } catch (error) {
+    console.error("ERROR CONNECTING TO MONGODB:", error);
+    process.exit(1);
+  }
+};
