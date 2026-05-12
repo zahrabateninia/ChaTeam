395a7a667e807a5f636a52fb7fe7a317787cdbc3
zahrabateninia <zahrabatenin@gmail.com>
2026-05-12 17:10:20 +0330

    Add an endpoint for checking if the user is authenticated or not

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -8,5 +8,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post(("/logout", logout));
 router.put(("/update-profile",protectRoute, updateProfile));
+router.put("/check", protectRoute, (req, res) => res.status(200).json(req.user));
+
 
 export default router;
