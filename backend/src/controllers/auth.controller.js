94adc48f3f114005d9e2866ce9353c58b9afbf47
zahrabateninia <zahrabatenin@gmail.com>
2026-05-13 20:32:56 +0330

    fix: Exclude user's password when updating profile

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -112,8 +112,9 @@ export const updateProfile = async (req, res) => {
     const updatedUser = await User.findByIdAndUpdate(
       userId,
       { profilePic: uploadResponse.secure_url },
-      { new: true },
-    );
+      { new: true }
+    ).select("-password");
+    
     res.status(200).json(updatedUser);
   } catch (error) {
     console.log("Error in updating profile pic: ", error);
