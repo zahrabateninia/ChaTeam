d0766060529f96cc44cac851d88c9b7e4725764c
zahrabateninia <zahrabatenin@gmail.com>
2026-05-14 18:57:14 +0330

    Implement getMessagesByUserId controller

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,33 @@
+import Message from "../modals/Message.js";
+import User from "../modals/User.js";
+
+export const getAllContacts = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getAllContacts", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getMessagesByUserId = async (req, res) => {
+  try {
+    const myId = req.user._id;
+    const { id: userToChatId } = req.params;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiver: myId },
+      ],
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessagesByUserId", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getAllContacts } from "../controllers/message.controller";
+import { getAllContacts, getMessagesByUserId } from "../controllers/message.controller";
 
 const router = express.Router();
 router.get("/contacts", protectedRoute, getAllContacts);
 // router.get("/chats", getChatPartners); // ppl you've chatted with
-// router.get("/:id", getMessagesByUserId); // all the messages between u and a chosen user
+router.get("/:id", getMessagesByUserId); // all the messages between u and a chosen user
 // router.post("/send/:id", sendMessage);
 
 export default router;
