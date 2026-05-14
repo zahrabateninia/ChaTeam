3832b0f1fe079c5e680ded3d1f2b6ba78903ddc4
zahrabateninia <zahrabatenin@gmail.com>
2026-05-14 19:13:53 +0330

    Implement (partly) sendMessage controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -9,7 +9,7 @@ export const getAllContacts = async (req, res) => {
     }).select("-password");
     res.status(200).json(filteredUsers);
   } catch (error) {
-    console.log("Error in getAllContacts", error);
+    console.log("Error in getAllContacts controller", error);
     res.status(500).json({ message: "Server error" });
   }
 };
@@ -27,7 +27,36 @@ export const getMessagesByUserId = async (req, res) => {
     });
     res.status(200).json(messages);
   } catch (error) {
-    console.log("Error in getMessagesByUserId", error);
+    console.log("Error in getMessagesByUserId controller", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { id: receiverId } = req.params;
+    const senderId = req.user.id;
+    const { text, image } = req.body;
+
+    let imageUrl;
+    if (image) {
+      // todo
+      // store the image in the cloud
+      // and get the secure url and assign it to the imageUrl
+    }
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save(); // save it to the db
+
+    res.status(201).json(newMessage); // 201: sth created successfully
+    // todo: send message in real-time if user is online - socket.it
+  } catch (error) {
+    console.log("Error in sendMessage controller", error);
     res.status(500).json({ message: "Server error" });
   }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getAllContacts, getMessagesByUserId } from "../controllers/message.controller";
+import {
+  getAllContacts,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller";
 
 const router = express.Router();
 router.get("/contacts", protectedRoute, getAllContacts);
-// router.get("/chats", getChatPartners); // ppl you've chatted with
-router.get("/:id", getMessagesByUserId); // all the messages between u and a chosen user
-// router.post("/send/:id", sendMessage);
+router.get("/chats", protectedRoute, getChatPartners); // ppl you've chatted with
+router.get("/:id", protectedRoute, getMessagesByUserId); // all the messages between u and a chosen user
+router.post("/send/:id", protectedRoute, sendMessage);
 
 export default router;
